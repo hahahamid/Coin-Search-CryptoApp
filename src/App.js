87cac00c39bb7dec1f34cb59e63005daacbe510e
Miran Hamid <miranhamid2002@gmail.com>
2023-08-29 15:53:38 +0530

    Change Fonts in Hero Section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Coin from "./pages/Coin";
 
+
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route id="home" path="/" element={<Home />} />
         <Route path="/coin" element={<Coin />}>
           <Route path=":coinId" element={<Coin />}></Route>
         </Route>
